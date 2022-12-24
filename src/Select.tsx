6f8a588bcd6558c791f01fb766b5544e2dd5ec5d
adamhinton <adam.d.hinton@gmail.com>
2022-12-23 21:18:42 -0500

    type defs Select.tsx

---
+++ src/Select.tsx
@@ -0,0 +1,12 @@
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+type SelectProps = {
+  options: SelectOption[];
+  onChange: (value: SelectOption | undefined) => void;
+  value?: SelectOption;
+};
+
+export function Select({ value, onChange, options }: SelectProps) {}
